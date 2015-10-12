5ce7304e08d8898e5cd49807c61945b063dff1f3
robert <rhsu123@gmail.com>
2015-10-12 12:46:52 -0700

    fixed up some formatting

--- src/coreLibrary/ShuntingYard.java
+++ src/coreLibrary/ShuntingYard.java
@@ -13,7 +13,7 @@ public class ShuntingYard
 	 * @param operator
 	 * @return the precedence of the operator
 	 */
-    private static int getPrecedence(String operator)
+	private static int getPrecedence(String operator)
 	{	
 		if((operator.equals("*")) || (operator.equals("/")))
 		{
@@ -22,11 +22,11 @@ public class ShuntingYard
 		else if ((operator.equals("+")) || (operator.equals("-")))
 		{
 			return 2;
-        }
-        else 
-        {
-            return 0;
-        }
+		}
+		else 
+		{
+			return 0;
+		}
 	}
 	
 	/**
@@ -38,56 +38,53 @@ public class ShuntingYard
 		//TODO: Add in other operators. This function is incomplete
 		return true;
 	}
-	
+
 	/**
 	 * @param expressionString the string to be parsed
 	 * @return a LinkedList of the expression in Reverse Polish Notation
 	 */
 	private static LinkedList<String> Parse(String expressionString)
 	{
-        Stack<String>s_operator = new Stack<String>();
-        LinkedList<String>s_output = new LinkedList<String>();
-        
+		Stack<String>s_operator = new Stack<String>();
+		LinkedList<String>s_output = new LinkedList<String>();
+
 		//Split the string into tokens. 
 		String[] expression = expressionString.split("\\s+");
 
 		//Begin the Shunting Yard Alogrithm
-		for (int i = 0; i < expression.length; i++)
-        {
-			//Read a token
-			String c = expression[i];
-            
+		for (String c : expression) 
+		{
 			//If the token is a number, then add it to the output queue
 			if(PublicFunctions.isInteger(c))
 			{
 				s_output.offer(c);
 			}
-            //Paranthesis Handler
-            else if(c.equals("("))
-            {
-                s_operator.push(c);
-            }
-            else if(c.equals(")"))
-            {
-                while(!s_operator.peek().equals("("))
-                {
+			//Paranthesis Handler
+			else if(c.equals("("))
+			{
+				s_operator.push(c);
+			}
+			else if(c.equals(")"))
+			{
+				while(!s_operator.peek().equals("("))
+				{
 					s_output.offer(s_operator.pop());
-                }
-                s_operator.pop(); //pop the left paran off
-            }
+				}
+				s_operator.pop(); //pop the left paran off
+			}
 
 			//If the token is an operator - o1
 			else
-            {
-                String o1 = c; //For consistency purposes. 
-                
+			{
+				String o1 = c; //For consistency purposes.
+
 				//while there is an operator-o2 at the top of the stack
 				while(!s_operator.empty())
 				{
 					String o2 = s_operator.peek();
 					int o1_p = getPrecedence(o1);
 					int o2_p = getPrecedence(o2);
-					
+
 					if(( isLeftAssociative(o1) && (o1_p <= o2_p) ) || (o1_p < o2_p))
 					{
 						s_output.offer(s_operator.pop());
@@ -98,39 +95,34 @@ public class ShuntingYard
 					}
 				}
 				s_operator.push(o1);
-            }
-        }	
-				
+			}
+		}	
+
 		while(!s_operator.empty())
 		{
 			s_output.offer(s_operator.pop());
 		}
-        
-        return s_output;
+
+		return s_output;
 	}
-    
+
 	/**
 	 * @deprecated
 	 * @param expressionString the expression to be parsed
 	 * @return the expression string parsed to Reverse Polish Notation
 	 */
-    public static String getString(String expressionString)
-    {
-        return Parse(expressionString).toString();
-    }
-     
+	public static String getString(String expressionString)
+	{
+		return Parse(expressionString).toString();
+	}
+
 	/**
 	 * 
 	 * @param expressionString the expression to be parsed
 	 * @return the expression string parsed to Reverse Polish Notation
 	 */
-    public static LinkedList<String> getList(String expressionString)
-    {
-        return Parse(expressionString);
-    }
-    
-    public static void main(String[] args)
-    {
-		
-    }
+	public static LinkedList<String> getList(String expressionString)
+	{
+		return Parse(expressionString);
+	}
 }
